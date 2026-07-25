4b974e92fdd2b143a2e07807c80d345ed9431b08
wsejagad-arch <wsejagad@gmail.com>
2026-07-25 23:26:18 +0700

    Update database configuration for cPanel

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Konfigurasi Database - Sesuaikan saat upload ke cPanel
 $host = "localhost";
-$user = "root";       // Ganti dengan username database cPanel (contoh: user_dbsekolah)
-$pass = "";           // Ganti dengan password database cPanel
-$db   = "sekolah_db"; // Ganti dengan nama database cPanel (contoh: user_sekolahdb)
+$user = "smasumb1_web1";       // Ganti dengan username database cPanel (contoh: user_dbsekolah)
+$pass = "W@hyu123465";           // Ganti dengan password database cPanel
+$db   = "smasumb1_smanis1"; // Ganti dengan nama database cPanel (contoh: user_sekolahdb)
 
 $conn = new mysqli($host, $user, $pass, $db);
 
