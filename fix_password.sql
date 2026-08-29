37bda514526db28fb501d813d289f8ee13fa0c15
wsejagad-arch <wsejagad@gmail.com>
2026-08-29 15:57:55 +0700

    Add SQL script to fix admin password in production

---
+++ fix_password.sql
@@ -0,0 +1,2 @@
+-- Update password admin dengan hash yang benar untuk password: W@hyu123
+UPDATE `admin` SET `password` = '$2y$10$AjIJVQqB7kXjEIob77Wut.HzFGLmh5cK/hObFc9ihyNzj462F1ffi' WHERE `username` = 'admin';
